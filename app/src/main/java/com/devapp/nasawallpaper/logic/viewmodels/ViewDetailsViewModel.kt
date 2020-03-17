34f0146ea05aa59e4b9f4ce97b059b295c8ef99d
devapp <arsenyzp@gmail.com>
2020-03-17 12:20:50 +0300

    details view layout

--- app/src/main/java/com/devapp/nasawallpaper/logic/viewmodels/ViewDetailsViewModel.kt
+++ app/src/main/java/com/devapp/nasawallpaper/logic/viewmodels/ViewDetailsViewModel.kt
@@ -1,18 +1,22 @@
 package com.devapp.nasawallpaper.logic.viewmodels
 
 import android.app.Application
+import android.app.DownloadManager
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.text.TextUtils
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.devapp.nasawallpaper.logic.controllers.DownloadImageController
 import com.devapp.nasawallpaper.logic.entity.EntityImage
 import com.devapp.nasawallpaper.storage.database.DataRepository
 import com.devapp.nasawallpaper.storage.database.ImageMapper
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.devapp.nasawallpaper.utils.imageLoader.GlideDrawableLoader
+import kotlinx.coroutines.*
+import java.io.File
 
-class ViewDetailsViewModel(private val app: Application, private val dataRepository: DataRepository) : BaseViewModel(app) {
+class ViewDetailsViewModel(private val app: Application, private val dataRepository: DataRepository, private val downloadController: DownloadImageController) : BaseViewModel(app) {
 
     var imageId: Int? = null
     set(value) {
@@ -20,15 +24,17 @@ class ViewDetailsViewModel(private val app: Application, private val dataReposit
         field = value
     }
 
+    private var downloadImageJob: Job? = null
     private val mapper = ImageMapper()
     val imageInfo = MutableLiveData<EntityImage>()
 
     companion object {
         fun createFactory(
             application: Application,
-            dataRepository: DataRepository
+            dataRepository: DataRepository,
+            downloadController: DownloadImageController
         ) : ViewModelProvider.Factory {
-            return ViewModelFactory(application, dataRepository)
+            return ViewModelFactory(application, dataRepository, downloadController)
         }
     }
 
@@ -45,10 +51,34 @@ class ViewDetailsViewModel(private val app: Application, private val dataReposit
         }
     }
 
+    suspend fun getImageDrawable(): Drawable?{
+        val item = imageInfo.value ?: return null
+
+        if(TextUtils.isEmpty(item.localPath)){
+            downloadController.downloadImage(item)
+            return null
+        }
+        val f = File(item.localPath)
+        if (!f.exists()){
+            downloadController.downloadImage(item)
+            return null
+        }
+        val loader =
+            GlideDrawableLoader(
+                app.applicationContext
+            )
+        return loader.load(Uri.fromFile(f), 1000)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        downloadImageJob?.cancel()
+    }
+
     @Suppress("UNCHECKED_CAST")
-    class ViewModelFactory(private val application: Application, private val dataRepository: DataRepository): ViewModelProvider.NewInstanceFactory() {
+    class ViewModelFactory(private val application: Application, private val dataRepository: DataRepository, private val downloadController: DownloadImageController): ViewModelProvider.NewInstanceFactory() {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            val viewModel = ViewDetailsViewModel(application, dataRepository)
+            val viewModel = ViewDetailsViewModel(application, dataRepository, downloadController)
             return viewModel as T
         }
     }
--- app/src/main/java/com/devapp/nasawallpaper/ui/fragments/ViewDetailsFragment.kt
+++ app/src/main/java/com/devapp/nasawallpaper/ui/fragments/ViewDetailsFragment.kt
@@ -6,10 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.devapp.nasawallpaper.App
 
 import com.devapp.nasawallpaper.R
 import com.devapp.nasawallpaper.logic.viewmodels.ViewDetailsViewModel
+import kotlinx.android.synthetic.main.view_details_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ViewDetailsFragment : NavigationFragment() {
 
@@ -31,11 +37,26 @@ class ViewDetailsFragment : NavigationFragment() {
         super.onActivityCreated(savedInstanceState)
         val args = ViewDetailsFragmentArgs.fromBundle(arguments!!)
         val app = (activity!!.application as App)
-        viewModel = ViewModelProviders.of(this, ViewDetailsViewModel.createFactory(app, app.dataRepository)).get(ViewDetailsViewModel::class.java)
+        viewModel = ViewModelProviders.of(this, ViewDetailsViewModel.createFactory(app, app.dataRepository, app.downloadController)).get(ViewDetailsViewModel::class.java)
         viewModel.imageId = args.imageId
 
         displayHome()
         setTitle(getString(R.string.app_name))
+
+        viewModel.imageInfo.observe(viewLifecycleOwner, Observer {
+            name.text = it.name
+            description.text = it.description
+            GlobalScope.launch {
+                withContext(Dispatchers.IO){
+                    val drawable = viewModel.getImageDrawable()
+                    drawable?.let {
+                        image.post {
+                            image.setImageDrawable(drawable)
+                        }
+                    }
+                }
+            }
+        })
     }
 
 }
